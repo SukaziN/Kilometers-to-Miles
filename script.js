b556c72cbaadc57ddea38d150fc5144d2b2c474e
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-30 06:16:59 +0200

    moved global declared selector variables into function and reduced decimal places to 2

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let kilometers = document.getElementById("kilometers").valueAsNumber;
-console.log(typeof kilometers); //returns number but the value still fetched is NaN instead of the user's input
-//kilometers = Number(kilometers);
-console.log(kilometers);
-//let button = document.getElementById("toMiles"); not really using this variable
-
 function convertDistance() {
-  let miles = kilometers * 1.60934;
-  //console.log(kilometers);
-  let message = console.log(`${kilometers} kms is equal to ${miles} miles`);
+  let kilometers = document.getElementById("kilometers").valueAsNumber;
+  console.log(typeof kilometers); //returns number but the value still fetched is NaN instead of the user's input
+  console.log(kilometers);
+  let miles = (kilometers * 1.60934).toFixed(2);
+  let message = `${kilometers} kms is equal to ${miles} miles`;
   let answer = document.createElement("p");
   answer.innerText = message;
   document.body.appendChild(answer);
 }
-//everything works, except the value of kilometers isn't being fetched from the input as it should
+
+//alright, so declaring all the selector variables globally made it so the input exists outside the click that invokes the function, therefore not registering the value as part of the calculation declared inside the function.
