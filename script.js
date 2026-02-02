e7962057426d1f5547a44fd8702d6b29bbfe2bba
SukaziN <nkosiyazisukazie@gmail.com>
2026-02-02 08:58:27 +0200

    cleaned up code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function convertDistance() {
   let kilometers = document.getElementById("kilometers").valueAsNumber;
-  console.log(typeof kilometers); //returns number but the value still fetched is NaN instead of the user's input
-  console.log(kilometers);
   let miles = (kilometers * 1.60934).toLocaleString("en-US");
   let message = `${kilometers} km is equal to ${miles} miles`;
   let answer = document.createElement("p");
@@ -9,4 +7,4 @@ function convertDistance() {
   document.body.appendChild(answer);
 }
 
-//alright, so declaring all the selector variables globally made it so the input exists outside the click that invokes the function, therefore not registering the value as part of the calculation declared inside the function.
+//alright, so declaring all the selector variables globally made it so the input exists outside the click that invokes the function, therefore not registering the value as part of the calculation declared inside the function. FIXED It!
